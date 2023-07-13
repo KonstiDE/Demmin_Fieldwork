1b16c33709d96bb72ee7737c04a6546a7bb57656
KonstiDE <konstantin.mueller1@gmx.de>
2023-07-13 22:55:23 +0200

    final update of README.MD and implemented data bootstrapping

--- main.R
+++ main.R
@@ -7,6 +7,7 @@ library(ggplot2)
 library(ggspatial)
 library(ggthemes)
 library(ggpubr)
+library(mvtnorm)
 
 esus <- st_read("data/esus.gpkg")
 fields <- st_read("data/fields.gpkg")
@@ -39,36 +40,67 @@ reg_df$ndvi <- ((reg_df$B8 - reg_df$B4) / (reg_df$B8 + reg_df$B4))
 
 reg_df <- reg_df[sample(1:nrow(reg_df)), ]
 
-trainIndex <- nrow(reg_df) * 0.8
+
+
+# Sythetic data generation via bootstrapping with randomness
+reg_df <- reg_df[,c("short", "ndvi", "evi", "weight_wet")]
+
+additional_indices <- sample(nrow(reg_df), size = 48, replace = TRUE)
+synthetic_data <- reg_df[,c("ndvi", "evi", "weight_wet")][additional_indices, ]
+randomness_factor <- 0.05
+
+for (col in c("ndvi", "evi", "weight_wet")) {
+  additional_data[[col]] <- additional_data[[col]] * (1 + randomness_factor * runif(48, min = -1, max = 1))
+}
+
+synthetic_data$short <- "synthetic"
+rownames(additional_data) <- NULL
+
+reg_df <- rbind(reg_df, synthetic_data)
+rownames(reg_df) <- NULL
+
+reg_df[sample(1:nrow(reg_df)), ]
+
+trainIndex <- nrow(reg_df) * 0.9
 train_df <- reg_df[0:trainIndex,]
-valid_df <- reg_df[(trainIndex):nrow(reg_df) + 1,]
+valid_df <- reg_df[(trainIndex + 1):nrow(reg_df),]
 
 train_df <- train_df[,c("short", "ndvi", "evi", "weight_wet")]
 valid_df <- valid_df[,c("short", "ndvi", "evi", "weight_wet")]
 
 fit_ndvi <- lm(formula = weight_wet ~ ndvi, data = train_df)
 fit_evi <- lm(formula = weight_wet ~ evi, data = train_df)
+fit_both <- lm(formula = weight_wet ~ ndvi + evi, data = train_df)
 
 preds_ndvi <- predict(fit_ndvi, valid_df)
 preds_evi <- predict(fit_evi, valid_df)
+preds_both <- predict(fit_both, valid_df)
 
 mean_ndvi <- mean(valid_df$weight_wet - preds_ndvi)
 mean_evi <- mean(valid_df$weight_wet - preds_evi)
+mean_both <- mean(valid_df$weight_wet - preds_both)
 
 preds_ndvi <- as.numeric(preds_ndvi)
 preds_evi <- as.numeric(preds_evi)
-preds_df <- data.frame(x = seq_along(preds_ndvi), y_ndvi = preds_ndvi, weight_wet = valid_df$weight_wet, y_evi = preds_evi)
+preds_both <- as.numeric(preds_both)
+preds_df <- data.frame(
+  x = seq_along(preds_ndvi),
+  weight_wet = valid_df$weight_wet,
+  y_ndvi = preds_ndvi,
+  y_evi = preds_evi,
+  y_both = preds_both
+)
 
-df_long <- tidyr::pivot_longer(preds_df, cols = c(y_ndvi, weight_wet, y_evi))
-df_long$name <- factor(df_long$name, levels = c("y_ndvi", "weight_wet", "y_evi"))
+df_long <- tidyr::pivot_longer(preds_df, cols = c(weight_wet, y_ndvi, y_evi, y_both))
+df_long$name <- factor(df_long$name, levels = c("weight_wet","y_ndvi", "y_evi", "y_both"))
 
 ggplot(df_long, aes(x = as.factor(x), y = value, fill = name)) +
   geom_bar(stat = "identity", position = "dodge", width = 0.7) +
   xlab("SSUS") +
   ylab("AGB in g / 5 stems") +
   scale_fill_manual(
-    values = c("salmon", "#017825", "#0073C2"),
-    labels = c("Est. NDVI", "True AGB", "Est. EVI"),
+    values = c("#017825","salmon", "#0073C2", "#f5c242"),
+    labels = c("True AGB","Est. NDVI", "Est. EVI", "Est. Both"),
     guide_colorbar(title = "")
   ) +
   scale_x_discrete(breaks = seq_along(valid_df$short), labels = valid_df$short) +
@@ -97,7 +129,7 @@ ggplot() +
 
 
 # Predict with evi
-preds_fields_evi <- predict(fit_evi, sen2pixels)
+preds_fields_evi <- predict(sen2pixels, fit_ndvi)
 
 sentinel2_pred_evi <- sentinel2_fields[[1]]
 sentinel2_pred_evi[!is.na(sentinel2_pred_evi)] <- as.numeric(preds_fields_evi)[!is.na(preds_fields_evi)]
@@ -120,6 +152,33 @@ ggplot() +
   layer_spatial(data = diff) +
   scale_fill_gradientn(colors = c("blue", "white", "red"), na.value = "transparent")
 
+
+# Large Extent
+sentinel2_geom <- rast("data/sentinel2_larger.tif")
+fields_geom <- vect("data/fields_larger.gpkg")
+
+sentinel2_geom <- mask(sentinel2_geom, fields_geom)
+
+sen2large <- extract(sentinel2_geom, seq_along(0:length(values(sentinel2_geom))))
+colnames(sen2large) <- c("B4", "B3", "B2", "B8")
+
+sen2large$evi <- (2.5 * (sen2large$B8 - sen2large$B4)) / (sen2large$B8 + (2.4 * sen2large$B4) + 10000)
+sen2large$ndvi <- ((sen2large$B8 - sen2large$B4) / (sen2large$B8 + sen2large$B4))
+
+pred_large <- predict(fit_ndvi, sen2large)
+
+sentinel2_pred_l <- sentinel2_geom[[1]]
+sentinel2_pred_l[!is.na(sentinel2_pred_l)] <- as.numeric(pred_large)[!is.na(pred_large)]
+sentinel2_pred_l[sentinel2_pred_l < 0] <- 0
+
+ggplot() +
+  layer_spatial(data = sentinel2_pred_l) +
+  scale_fill_gradientn(
+    colors = c("black", "green"),
+    na.value = "transparent",
+    guide_colorbar(title = "AGB"),
+  ) +
+  xlab("Above Ground Biomass (Wet) in gram per 5 stems")
 
 
 
